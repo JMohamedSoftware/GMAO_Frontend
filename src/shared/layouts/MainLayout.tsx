ff9e4a60b6433050d8045fc6093c188c8add9a6b
Mohamed jbeli <jbelimohamed2025@gmail.com>
2026-09-18 19:32:13 +0100

    revert: restore MainLayout content div to original classes

--- src/shared/layouts/MainLayout.tsx
+++ src/shared/layouts/MainLayout.tsx
@@ -38,7 +38,7 @@ export const MainLayout: React.FC<MainLayoutProps> = ({ children, currentScreen,
       />
 
       {/* Screen Frame containing Navbar and active Screen */}
-      <div className="flex-1 flex flex-col min-h-screen p-4 gap-4 overflow-x-hidden ml-36 lg:ml-44">
+      <div className="flex-1 flex flex-col min-h-screen p-4 gap-4 overflow-x-hidden">
         {/* Glass Top Navbar */}
         <Navbar 
           currentScreen={currentScreen} 
